3567d1a7ae0729b9b4633caa44665b94d65fd2f9
Maxim <maxidebox@list.ru>
2025-01-11 00:52:08 +0400

    Обновление версии ng до 18, ленивая загрузка компонента

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,6 @@ import { SecondComponent } from './second/second.component';
 
 export const routes: Routes = [
     { path: 'first-component', component: FirstComponent },
-    { path: 'second-component', component: SecondComponent}
+    { path: 'second-component', component: SecondComponent},
+    { path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent) },
 ];
